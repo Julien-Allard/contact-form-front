d22a6861bbdcb140d6e2c71960e671844e605d61
Julien-Allard <julalldev@gmail.com>
2022-01-28 17:06:50 +0100

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector("#contactForm")
     .addEventListener("submit", async (e) => {
-      e.preventDefault();
-      const data = {
-        firstname: document.querySelector("#firstName").value,
-        lastname: document.querySelector("#lastName").value,
-        email: document.querySelector("#email").value,
-        subject: document.querySelector("#subject").value,
-        message: document.querySelector("#message").value,
-      };
+      try {
+        e.preventDefault();
+        const data = {
+          firstname: document.querySelector("#firstName").value,
+          lastname: document.querySelector("#lastName").value,
+          email: document.querySelector("#email").value,
+          subject: document.querySelector("#subject").value,
+          message: document.querySelector("#message").value,
+        };
 
-      const response = await axios.post(
-        "https://contact-form-le-reacteur.herokuapp.com/form",
-        data
-      );
+        const response = await axios.post(
+          "https://contact-form-le-reacteur.herokuapp.com/form",
+          data
+        );
+      } catch (error) {
+        console.log("ERROR ==> ", error.message);
+        console.log("ERROR RESPONSE ==> ", error.response);
+      }
     });
 });
